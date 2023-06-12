827389fa8ff7acd05f7089ee4a94ff11986b1fd4
cristian.albu <albucristian95p@gmail.com>
2023-06-12 16:54:59 +0300

    initial commit

---
+++ app/__tests__/home.test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Home from "../page";
+
+describe("Home", () => {
+    it("renders a heading", () => {
+        render(<Home />);
+
+        const heading = screen.getByRole("heading");
+
+        expect(heading).toBeInTheDocument();
+    });
+});
